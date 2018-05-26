9d82691cdfa59af566a2a1c84b7a8d5dbecaa9f0
Juan Montaño <j.montano.m2@gmail.com>
2018-05-26 11:46:42 -0400

    comenzando el proyecto

---
+++ proyectoFinalCurso/src/test/java/com/montano/juan/proyectoFinalCurso/ProyectoFinalCursoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.montano.juan.proyectoFinalCurso;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProyectoFinalCursoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
